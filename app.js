d80f19decfcbb351f133d5039e2fb03c46ad7080
Arielle Loring <arieloring@gmail.com>
2020-02-01 13:43:26 -0700

    extract values

--- app.js
+++ app.js
@@ -11,15 +11,38 @@ var budgetController = (function(){
 // UI CONTROLLER
 var UIController = (function(){
 
-  // Some code
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputButton: '.add__btn'
+  }
+
+  return {
+    getInput: function(){
+        return {
+          type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+          description: document.querySelector(DOMstrings.inputDescription).value,
+          value: document.querySelector(DOMstrings.inputValue).value
+        }
+    },
+
+    getDOMStrings: function(){
+      return DOMstrings;
+    }
+  };
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+  var DOM = UICtrl.getDOMStrings()
+
   var ctrlAddItem = function() {
     // 1. Get input data
+      var input = UICtrl.getInput();
+      console.log(input);
 
     // 2. Add item to budget controller
 
@@ -28,17 +51,17 @@ var controller = (function(budgetCtrl, UICtrl){
     // 4. Calculate the budget
 
     // 5. Display the budget on UI
-    console.log('Yay');
+
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event){
 
     if(event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
-
     }
+
 
   });
 
