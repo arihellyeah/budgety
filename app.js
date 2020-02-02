613cb2572f7f908e81ae35bbff9775e06de874ac
Arielle Loring <arieloring@gmail.com>
2020-02-01 20:35:29 -0700

    clear input fields after add

--- app.js
+++ app.js
@@ -102,6 +102,20 @@ var UIController = (function(){
 
     },
 
+    clearFields: function(){
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' +
+      DOMstrings.inputValue);
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array){
+        current.value = "";
+      });
+      fieldsArr[0].focus();
+    },
+
     getDOMStrings: function(){
       return DOMstrings;
     }
@@ -137,9 +151,13 @@ var controller = (function(budgetCtrl, UICtrl){
     // 3. Add the new item to the UI
       UICtrl.addListItem(newItem, input.type);
 
-    // 4. Calculate the budget
+    // 4. Clear fields
+      UICtrl.clearFields();
 
-    // 5. Display the budget on UI
+    // 5. Calculate the budget
+
+
+    // 6. Display the budget on UI
 
   };
 
