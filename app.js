db770d14256eb48cb32a9783372a567d9a8f0c35
Arielle Loring <arieloring@gmail.com>
2020-02-03 18:58:36 -0700

    add green red toggle to input

--- app.js
+++ app.js
@@ -184,6 +184,12 @@ var UIController = (function(){
 
   };
 
+  var nodeListForEach = function(list, callback){
+    for (var i = 0; i < list.length; i++){
+      callback(list[i], i);
+    }
+  };
+
   return {
     getInput: function(){
         return {
@@ -256,12 +262,6 @@ var UIController = (function(){
 
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-      var nodeListForEach = function(list, callback){
-        for (var i = 0; i < list.length; i++){
-          callback(list[i], i);
-        }
-      };
-
       nodeListForEach(fields, function(current, index){
 
         if (percentages[index] > 0) {
@@ -284,10 +284,25 @@ var UIController = (function(){
       year = now.getFullYear();
       document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
 
+    },
+
+    changedType: function() {
+
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' +
+        DOMstrings.inputDescription + ',' +
+        DOMstrings.inputValue
+      );
+
+      nodeListForEach(fields, function(cur){
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMstrings.inputButton).classList.toggle('red');
 
     },
 
-    getDOMStrings: function(){
+    getDOMStrings: function() {
       return DOMstrings;
     }
   };
@@ -311,6 +326,8 @@ var controller = (function(budgetCtrl, UICtrl){
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
   };
 
 
