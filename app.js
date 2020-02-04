2b2c81ca2d8d9886f034e011acc88af22c2420c2
Arielle Loring <arieloring@gmail.com>
2020-02-03 18:47:11 -0700

    add date to header

--- app.js
+++ app.js
@@ -154,7 +154,8 @@ var UIController = (function(){
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month'
 
   };
 
@@ -272,6 +273,20 @@ var UIController = (function(){
 
     },
 
+    displayMonth: function() {
+      var now, year, month, months;
+      now = new Date();
+
+      months = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July',
+      'August', 'September', 'October', 'November', 'December'];
+      month = now.getMonth();
+
+      year = now.getFullYear();
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+
+    },
+
     getDOMStrings: function(){
       return DOMstrings;
     }
@@ -376,6 +391,7 @@ var controller = (function(budgetCtrl, UICtrl){
 
   return {
     init: function(){
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
           budget: 0,
           totalInc: 0,
